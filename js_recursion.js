04cea493885c400b6f9c2e149b7ed251efbf1905
Malcolm Ahoy <colmahoy@gmail.com>
2014-04-02 21:53:26 -0700

    Initial commit with js and ruby exercises

---
+++ js_recursion.js
@@ -0,0 +1,130 @@
+var range = function(start, stop) {
+  if(start === stop){
+    return stop;
+  } else {
+    var results = [start];
+    results = results.concat(range(start + 1, stop));
+  }
+  return results;
+}
+
+// console.log(range(1, 5));
+
+var exponentiation = function(base, power) {
+  if (power === 0) {
+    return 1
+  };
+
+  return base * exponentiation(base, power - 1);
+};
+
+// console.log(exponentiation(2, 4));
+
+var fibonacci = function(num) {
+  switch (num) {
+    case 0:
+      return [];
+      break;
+    case 1:
+      return [1];
+      break;
+    case 2:
+      return [1, 1];
+      break;
+    default:
+      var results = fibonacci(num - 1);
+      results.push(results[results.length - 1] + results[results.length - 2]);
+      return results;
+  };
+};
+
+// console.log(fibonacci(7));
+
+var binarySearch = function(arr, target) {
+  mid = Math.floor(arr.length / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return binarySearch(arr.slice(0, mid), target);
+  } else if (arr[mid] < target) {
+    return mid + 1 + binarySearch(arr.slice(mid + 1), target);
+  } else {
+    return undefined;
+  }
+};
+
+//console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 0));
+
+var makeChange = function(value, coins) {
+  if (coins === undefined && typeof(coins) !== "array") {
+    coins = [10, 7, 1];
+  }
+
+  var results = [];
+
+  if(value === 0) {
+    return [];
+  } else {
+
+    for (var i in coins) {
+
+      if (coins[i] <= value) {
+        results.push([coins[i]].concat(makeChange(value - coins[i], coins)[0]));
+      }
+    }
+  }
+};
+
+// console.log(makeChange(22));
+
+var merge = function(left, right) {
+  var merged = [];
+
+  while (left.length > 0 && right.length > 0) {
+    var nextItem = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(nextItem);
+  }
+
+  return merged.concat(left).concat(right);
+};
+
+var mergeSort = function(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    var middle = Math.floor(array.length / 2);
+
+    var left = mergeSort(array.slice(0, middle));
+    var right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
+  }
+};
+
+var subsets = function(array) {
+  if (array.length === 0) {
+    return [[]];
+  }
+
+  var lastElement = array[0];
+  var subSubsets = subsets(array.slice(1));
+
+  var newSubsets = subSubsets.map(function(subSubset) {
+    return subSubset.concat(lastElement);
+  });
+
+  return newSubsets.concat(subSubsets);
+};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ruby_recursion.rb
@@ -0,0 +1,126 @@
+def range_it(start_num, end_num)
+  result = []
+  (start_num..end_num).each do |num|
+    result << num
+  end
+  result
+end
+
+def range(num1, num2)
+  return [num2] if num1 == num2
+
+  [num1] + range(num1+1, num2)
+end
+
+def exponent1(base, number)
+  return 1 if number == 0
+
+  base * exponent1(base, number - 1)
+end
+
+
+def exponent2(base, number)
+  return base if number == 1
+
+  if number.even?
+    exponent2(base, number / 2) ** 2
+  elsif number.odd?
+    base * (exponent2(base, (number - 1)/2) ** 2)
+  end
+
+end
+
+def deep_dup(arr)
+  results = []
+
+  arr.each do |elem|
+    if elem.is_a?(Array)
+      results << deep_dup(elem)
+    else
+      results << elem
+    end
+  end
+
+  results
+end
+
+def fib(n)
+  return [] if n <= 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  base = fib(n-1)
+  base << ( base[-1] + base[-2] )
+end
+
+def bsearch(array, target)
+  return nil if array.empty?
+
+  middle = array.length / 2
+  if target == array[middle]
+    middle
+  elsif target < array[middle]
+    index = bsearch(array[0...middle], target)
+  else
+    index = bsearch(array[middle + 1..-1], target)
+    return nil if index.nil?
+    middle + 1 + index
+  end
+end
+
+def make_change(amt, coins = [25, 10, 5, 1])
+  return [amt] if amt == coins.last
+  return [] if amt == 0
+
+  if amt < coins.first
+    coins.shift
+    make_change(amt, coins)
+  else
+    [coins.first] + make_change(amt-coins.first, coins)
+  end
+end
+
+def merge_sort(array)
+  return array if array.length == 1
+
+  middle = (array.length / 2 == 1 ? 0 : array.length / 2)
+  merge(merge_sort(array[0..middle]), merge_sort(array[middle+1..-1]))
+end
+
+def merge(array1, array2)
+  merged_array = []
+
+  until array1.empty? || array2.empty?
+    p array1, array2
+    merged_array << (array1.first > array2.first ? array2.shift : array1.shift)
+  end
+
+  if array1.empty?
+    merged_array << array2.shift until array2.empty?
+  else
+    merged_array << array1.shift until array1.empty?
+  end
+
+  merged_array
+end
+
+def subsets(arr)
+  return [[]] if arr == []
+
+
+  subs = subsets(arr[0..-2])
+  last = arr[-1]
+  result = []
+
+  subs.each do |subset|
+    result << subset + [last]
+  end
+
+  result + subs
+end
+
+
+
+
+
+
